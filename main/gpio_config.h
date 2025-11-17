4c8eb34f68196d51ea80b5dda7fb40e7f603a58c
Marianannn <mariana.leticia05@gmail.com>
2025-11-17 12:10:07 -0300

    adicionando gpio_config

---
+++ main/gpio_config.h
@@ -0,0 +1,28 @@
+#pragma once
+
+// Motores 
+#define MOTOR1_PWM_PIN 22
+#define MOTOR2_PWM_PIN 23
+
+
+// LED
+#define LED1_PIN
+
+
+// MULTIPLEX
+#define MUX_S0_PIN 12
+#define MUX_S1_PIN 13
+#define MUX_S2_PIN 14
+#define MUX_S3_PIN 27
+#define MUX_Z 26
+
+
+// REGULADOR DE TENSÃO
+#define REGULADOR_TENSAO 21
+
+
+// JOYSTICK
+#define JOYSTICK_DIR_Y 25
+#define JOYSTICK_ESQ_Y 32
+#define JOYSTICK_DIR_X 33
+#define JOYSTICK_ESQ_X 35
