8eada274df2640614319d00e018670a80b76254e
Marianannn <mariana.leticia05@gmail.com>
2025-11-17 11:58:01 -0300

    fix: arrumando timers

--- main/main.c
+++ main/main.c
@@ -13,7 +13,7 @@
 #include "driver/gpio.h"
 #include "driver/ledc.h"
 
-#define MOTOR_1 2
+#define MOTOR_1 2 // por enquanto está com esses numeros por eu estar testando através de um led
 #define MOTOR_2 2
 
 // O duty (duty cycle) é a porcentagem de tempo que o sinal PWM fica em nível alto (1) dentro de um ciclo completo.
@@ -31,22 +31,22 @@ void setup_channel_motores(){
         .timer_sel = LEDC_TIMER_0,
         .duty = 0, // aperentemente determina o numero de inicio do duty
         .hpoint = 0// É o ponto inicial do contador do PWM dentro do ciclo. Se você tem vários canais PWM na mesma frequência, pode usar diferentes hpoint para evitar ruído ou picos de corrente.
-    }
+    };
 
     ledc_channel_config(&chanel_config_motor_1);
 
     // ====================================MOTOR 2 ==================================================================
-    ledc_channel_config_t chanel_config_motor_1 = {
-        .gpio_num = MOTOR_2,
-        .speed_mode = LEDC_LOW_SPEED_MODE,
-        .channel = LEDC_CHANNEL_1,
-        .intr_type = LEDC_INTR_DISABLE, 
-        .timer_sel = LEDC_TIMER_1,
-        .duty = 0, 
-        .hpoint = 0
-    }
+    // ledc_channel_config_t chanel_config_motor_2 = {
+    //     .gpio_num = MOTOR_2,
+    //     .speed_mode = LEDC_LOW_SPEED_MODE,
+    //     .channel = LEDC_CHANNEL_1,
+    //     .intr_type = LEDC_INTR_DISABLE, 
+    //     .timer_sel = LEDC_TIMER_1,
+    //     .duty = 0, 
+    //     .hpoint = 0
+    // };
 
-    ledc_channel_config(&chanel_config_motor_1);
+    // ledc_channel_config(&chanel_config_motor_2);
 }
 
 void setup_timer_motores(){
@@ -56,24 +56,24 @@ void setup_timer_motores(){
     // ====================================MOTOR 1 ==================================================================
     ledc_timer_config_t timer_config_motor_1 = {
         .speed_mode = LEDC_LOW_SPEED_MODE, // define qual grupo de temporizadores/canais o hardware do PWM vai usar
-        .timer_num =  LEDC_TIMER_1, // qual dos timers será utilizado durante a operação
+        .timer_num =  LEDC_TIMER_0, // qual dos timers será utilizado durante a operação
         .freq_hz = 1000, // frequencia em heartz do timer
-        .duty_resolution = LEDC_TIMER_8_BIT// Ele ajusta quantos bits o contador interno do PWM vai ter 8 bits -> 255; 10 bits-> 1023 etc.
+        .duty_resolution = LEDC_TIMER_8_BIT, // Ele ajusta quantos bits o contador interno do PWM vai ter 8 bits -> 255; 10 bits-> 1023 etc.
         .clk_cfg = LEDC_AUTO_CLK // diz ao ESP32 qual clock interno o periférico PWM (LEDC) vai usar — e, no caso do AUTO, ele escolhe automaticamente o melhor clock disponível.
-    }
+    };
 
     ledc_timer_config(&timer_config_motor_1);
     
     // ====================================MOTOR 2 ==================================================================
-    ledc_timer_config_t timer_config_motor_2 = {
-        .speed_mode = LEDC_LOW_SPEED_MODE,
-        .timer_num =  LEDC_TIMER_0,
-        .freq_hz = 1000,
-        .duty_resolution = LEDC_TIMER_8_BIT
-        .clk_cfg = LEDC_AUTO_CLK
-    }
+    // ledc_timer_config_t timer_config_motor_2 = {
+    //     .speed_mode = LEDC_LOW_SPEED_MODE,
+    //     .timer_num =  LEDC_TIMER_1,
+    //     .freq_hz = 1000,
+    //     .duty_resolution = LEDC_TIMER_8_BIT,
+    //     .clk_cfg = LEDC_AUTO_CLK
+    // };
 
-    ledc_timer_config(&timer_config_motor_2);
+    // ledc_timer_config(&timer_config_motor_2);
 
     ledc_fade_func_install(0); // habilida o efeito de fading
 }
@@ -86,9 +86,12 @@ void app_main(void){
     while(true)
     {
         ledc_set_fade_time_and_start(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 0, 1000, LEDC_FADE_WAIT_DONE);
-        ledc_set_fade_time_and_start(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 255, 1000, LEDC_FADE_WAIT_DONE);
+        // ledc_set_fade_time_and_start(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, 0, 1000, LEDC_FADE_WAIT_DONE);
+        printf("led desligado!!!\n");
+  
+        ledc_set_fade_time_and_start(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 255, 2000, LEDC_FADE_WAIT_DONE);
+        // ledc_set_fade_time_and_start(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, 255, 2000, LEDC_FADE_WAIT_DONE);
+        printf("led ligado!!!\n");
+
     }
-
-
-    
 }
